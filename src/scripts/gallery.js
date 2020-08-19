0a2e26054fac894417431e77f08969d7124a6d51
Ananev Andrei <ananevandrei11@gmail.com>
2020-08-19 12:48:47 +0400

    #5 finish creating test

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   let glr = document.querySelector('.company-gallery');
   let glrItems = glr.querySelectorAll('.company-gallery__figure');
+  let arrImg = [];
+
   if (glrItems) {
     console.log(glrItems.length);
     for (let i=0; i < glrItems.length; i++) {
@@ -8,37 +10,54 @@ window.onload = function() {
       glrTotal.innerText = glrItems.length;
       let glrCount = glrItems[i].querySelector('.count-item');
       glrCount.innerText = i+1;
+
+      arrImg.push(glrItems[i].querySelector('.company-gallery__img').getAttribute('src'));
     }
   }
 
-  glr.onclick = function showItem(event) {
+  glr.onclick = function showGlr(event) {
     let target = event.target;
     if (target.tagName != 'BUTTON') return;
-    let backdrop = document.createElement('div');
-    document.body.appendChild(backdrop);
-    backdrop.style.cssText = `position: fixed;
-                              background-color: rgba(66,66,66,0.5);
-                              width: 100vw;
-                              height: 100vh;
-                              z-index:30;`;
+    let backdrop = document.querySelector('.slider');
+    backdrop.classList.remove('close');
+    backdrop.classList.add('open');
 
-    let iconClose = document.createElement('span');
-    backdrop.appendChild(iconClose);
-    iconClose.innerText = "X";
-    iconClose.style.cssText = `position: fixed;
-                              color: #FFFFFF;
-                              width: 5vw;
-                              height: 5vh;
-                              top: 10px;
-                              right: 10px;
-                              cursor: pointer;
-                              z-index:40;`;
-                              
-    iconClose.onclick = function closeItem(event) {
+    let iconClose = document.querySelector('.slider-close');
+    iconClose.onclick = function closeGlr(event) {
       let target = event.target;
-      if (target.tagName != 'SPAN') return;
-      backdrop.style.display = "none";
+      if (target.tagName != 'BUTTON') return;
+      backdrop.classList.remove('open');
+      backdrop.classList.add('close');
+    }
+
+    let imgSrc = target.previousSibling.getAttribute('src');
+    let imgSlider = document.querySelector('.slider-img');
+    imgSlider.setAttribute('src', imgSrc);
+    
+    let imgCurrent = imgSlider.getAttribute('src');
+    let imgIndex = arrImg.indexOf(imgCurrent);
+    let sliderTotal = document.querySelector('.slider-count__total');
+    sliderTotal.innerText = arrImg.length;
+    let sliderItem = document.querySelector('.slider-count__item');
+    sliderItem.innerText = imgIndex+1;
+    
+    let arrowL = document.querySelector('.left');
+    arrowL.onclick = function showLeft() {
+      if (imgIndex == 0) {
+        imgIndex = arrImg.length;
+      }  
+      imgIndex--;
+      imgSlider.setAttribute('src', arrImg[imgIndex]);
+      sliderItem.innerText = imgIndex+1;
+    }
+    let arrowR = document.querySelector('.right');
+    arrowR.onclick = function showRight() { 
+      imgIndex++;
+      if (imgIndex == arrImg.length) {
+        imgIndex = 0;
+      } 
+      imgSlider.setAttribute('src', arrImg[imgIndex]);
+      sliderItem.innerText = imgIndex+1;
     }
   }; 
-
 };
